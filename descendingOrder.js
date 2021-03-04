949ae20683ebde134821970ea8bdf2d32e5ab85a
Jefry Kurniawan <jefry.kurniawan@nawadata.com>
2021-03-05 00:06:23 +0700

    + descendingOrder

---
+++ descendingOrder.js
@@ -0,0 +1,13 @@
+function descendingOrder(n) {
+    return parseInt(
+        n
+        .toString()
+        .split("")
+        .sort((a, b) => b - a)
+        .join("")
+    );
+}
+
+console.log(descendingOrder(0), 0);
+console.log(descendingOrder(1), 1);
+console.log(descendingOrder(123456789), 987654321);
